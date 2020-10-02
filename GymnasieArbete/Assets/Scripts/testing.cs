6a3f63a02bc562ab33dfbbc17f2893c49a2fb537
Samuel Eskilson <samuel.eskilson@gmail.com>
2020-10-02 15:34:34 +0200

    started ecs to work
    vs code can't access unity.entities namespace and world.active returns a definition missing error

---
+++ GymnasieArbete/Assets/Scripts/testing.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+
+public class testing : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        MakeEntity();
+    }
+
+    private void MakeEntity()
+    {
+        EntityManager entityManager = World.Active.EntityManager;
+
+    }
+}
