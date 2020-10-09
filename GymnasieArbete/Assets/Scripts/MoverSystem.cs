0a2cbcf9f12dcecbaa5292f55810a259f114583f
Samuel Eskilson <samuel.eskilson@gmail.com>
2020-10-09 18:06:12 +0200

    äntligen, ecs fnkar

---
+++ GymnasieArbete/Assets/Scripts/MoverSystem.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+using Unity.Transforms;
+using Unity.Mathematics;
+
+public class MoverSystem : ComponentSystem
+{
+    protected override void OnUpdate()
+    {
+        Entities.ForEach((ref Translation translation, ref MoveSpeedComponent moveSpeedComponent) =>
+        {
+            translation.Value.y += moveSpeedComponent.speed * Time.DeltaTime;
+
+            if (translation.Value.y > 4.5f)
+            {
+                moveSpeedComponent.speed = -math.abs(moveSpeedComponent.speed);
+            }
+
+            if (translation.Value.y < -4.5f)
+            {
+                moveSpeedComponent.speed = math.abs(moveSpeedComponent.speed);
+            }
+
+        });
+    }
+}
--- GymnasieArbete/Assets/Scripts/testing.cs
+++ GymnasieArbete/Assets/Scripts/testing.cs
@@ -2,18 +2,60 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Unity.Entities;
+using Unity.Transforms;
+using Unity.Collections;
+using Unity.Rendering;
 
-public class testing : MonoBehaviour
+public class Testing : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        MakeEntity();
-    }
+    [SerializeField] private Mesh mesh = null;
+    [SerializeField] private Material material = null;
 
-    private void MakeEntity()
+    private void Start()
     {
-        EntityManager entityManager = World.Active.EntityManager;
+        EntityManager entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
 
+        EntityArchetype entityArchetype = entityManager.CreateArchetype(
+            typeof(LevelComponent),
+            typeof(Translation),
+            typeof(RenderMesh),
+            typeof(LocalToWorld),
+            typeof(RenderBounds),
+            typeof(MoveSpeedComponent)
+            );
+
+        NativeArray<Entity> entityArray = new NativeArray<Entity>(20000, Allocator.Temp);
+        entityManager.CreateEntity(entityArchetype, entityArray);
+
+        for (int i = 0; i < entityArray.Length; i++)
+        {
+            Entity entity = entityArray[i];
+
+            entityManager.SetComponentData(entity,
+                new LevelComponent
+                {
+                    level = Random.Range(0, 10)
+                });
+
+            entityManager.SetComponentData(entity,
+                new MoveSpeedComponent
+                {
+                    speed = Random.Range(1f, 2f)
+                });
+
+            entityManager.SetComponentData(entity,
+                new Translation
+                {
+                    Value = new Unity.Mathematics.float3(Random.Range(-7.5f, 7.5f), 0, 0)
+                });
+
+            entityManager.SetSharedComponentData(entity, new RenderMesh
+            {
+                mesh = mesh,
+                material = material,
+            });
+        }
+
+        entityArray.Dispose();
     }
 }
