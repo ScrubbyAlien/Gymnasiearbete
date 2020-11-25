98607171bc9dbf6758763a36771bc25e66203af5
Samuel Eskilson <samuel.eskilson@gmail.com>
2020-11-25 11:38:42 +0100

    added infector script
    also added infected scripte but does nothing as of now

---
+++ GymnasieArbete(utan_ECS)/Assets/scripts/Infector.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Infector : MonoBehaviour
+{
+    public int numberToInfect;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            Infect(numberToInfect);
+        }
+    }
+
+    void Infect(int n)
+    {
+        GameObject[] students = GameObject.FindGameObjectsWithTag("Student");
+
+        for (int i = 0; i < numberToInfect; i++)
+        {
+            if (students[i].HasComponent<Infected>() == false)
+            {
+                students[i].AddComponent<Infected>();
+            }
+        }
+    }
+}
+
+public static class hasComponent
+{
+    public static bool HasComponent<T>(this GameObject flag) where T : Component
+    {
+        return flag.GetComponent<T>() != null;
+    }
+}
