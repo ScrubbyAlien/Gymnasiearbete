e2d1c40a663be588dacc4a0a20dbed066ed97bb9
Samuel Eskilson <samuel.eskilson@gmail.com>
2020-11-25 11:11:33 +0100

    movement och spawnern är klar

---
+++ GymnasieArbete(utan_ECS)/Assets/scripts/Spawner.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+
+    public GameObject studentPrefab;
+    GameObject newStudent;
+
+    public int[] waveSizes;
+    int wave;
+
+    void Start()
+    {
+        wave = 0;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            StartCoroutine(SpawnNextWave());
+        }
+    }
+
+    IEnumerator SpawnNextWave()
+    {
+        if (wave < waveSizes.Length)
+        {
+            for (int i = 0; i < waveSizes[wave]; i++)
+            {
+                newStudent = Instantiate<GameObject>(studentPrefab);
+                newStudent.transform.position = new Vector2(Random.Range(-8f, 8), Random.Range(-4f, 4f));
+                yield return null;
+            }
+            wave += 1;
+        }
+    }
+}
--- GymnasieArbete(utan_ECS)/Assets/scripts/StudentMovement.cs
+++ GymnasieArbete(utan_ECS)/Assets/scripts/StudentMovement.cs
@@ -4,8 +4,76 @@ using UnityEngine;
 
 public class StudentMovement : MonoBehaviour
 {
+    Rigidbody2D mRidigbody;
+    Camera mCamera;
+    Vector3 cameraPostion;
+
+    float borderWidth;
+    float borderHeight;
+    Vector2 cameraOffset;
+
+    Vector2 dir;
+    float speed;
+    float timeUntilNextDirChange;
+    float timeUntilNextSpeedChange;
+    float timeSinceLastDirChange;
+    float timeSinceLastSpeedChange;
+    Vector2 translation;
+
+    void Start()
+    {
+        mRidigbody = GetComponent<Rigidbody2D>();
+        mCamera = Camera.main;
+        cameraPostion = mCamera.transform.position;
+
+        borderWidth = mCamera.orthographicSize * mCamera.aspect * 2;
+        borderHeight = mCamera.orthographicSize * 2;
+        cameraOffset = new Vector2(cameraPostion.x, cameraPostion.y);
+
+        timeUntilNextDirChange = 0;
+        timeUntilNextSpeedChange = 0;
+
+        timeSinceLastDirChange = 0;
+        timeSinceLastSpeedChange = 0;
+
+    }
+
     void Update()
     {
-            
+        if (Time.time >= timeSinceLastDirChange + timeUntilNextDirChange)
+        {
+            dir = GetRandDir();
+            timeSinceLastDirChange = Time.time;
+            timeUntilNextDirChange = Random.Range(0.5f, 3f);
+            mRidigbody.velocity = dir * speed;
+        }
+        if (Time.time >= timeSinceLastSpeedChange + timeUntilNextSpeedChange)
+        {
+            speed = GetRandSpeed();
+            timeSinceLastSpeedChange = Time.time;
+            timeUntilNextSpeedChange = Random.Range(0.5f, 3f);
+            mRidigbody.velocity = dir * speed;
+        }
+
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Border")
+        {
+            mRidigbody.velocity *= -1;
+        }
+    }
+
+    Vector2 GetRandDir()
+    {
+        float randAngle = Random.Range(0f, 2 * Mathf.PI);
+        Vector2 d = new Vector2(Mathf.Cos(randAngle), Mathf.Sin(randAngle));
+        return d;
+    }
+
+    float GetRandSpeed()
+    {
+        return Random.Range(1f, 4f);
     }
 }
