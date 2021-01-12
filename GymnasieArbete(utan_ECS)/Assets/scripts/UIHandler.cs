4e62b58d283858d42d0db50f45f26695be0c484f
Samuel Eskilson <samuel.eskilson@gmail.com>
2021-01-12 19:15:07 +0100

    started on settings menu

--- GymnasieArbete(utan_ECS)/Assets/scripts/UIHandler.cs
+++ GymnasieArbete(utan_ECS)/Assets/scripts/UIHandler.cs
@@ -11,23 +11,31 @@ public class UIHandler : MonoBehaviour
     public Text numberRemoved;
     public Text density;
     public Text day;
+    public Text infRateValue;
 
     public Transform dayBar;
+
+    public GameObject settingsMenu;
 
     InfectionParameters p;
 
     void Update()
     {
         p = GameObject.FindObjectOfType<InfectionParameters>().GetComponent<InfectionParameters>();
+
+        //Text elements and daybar on main screen
         numberSusceptible.text = "Susceptible: " + GetStatCount("Sus").ToString();
         numberInfected.text = "Infected: " + GetStatCount("Inf").ToString();
         numberRemoved.text = "Removed: " + GetStatCount("Rem").ToString();
-        density.text = "Density: " + (GetStatCount("All") / Mathf.Pow(p.lengthOfSide, 2)).ToString("#.##") +
+        density.text = "Density: " + (GetStatCount("All") / Mathf.Pow(p.lengthOfSide, 2)).ToString("0.00") +
         " people/sqrm";
         day.text = "Day: " + Mathf.FloorToInt(Time.time / p.dayLength);
 
         float pBarScaleF = 3.25f * (Time.time % p.dayLength) / p.dayLength;
         dayBar.localScale = new Vector3(pBarScaleF, dayBar.localScale.y, 1);
+
+        //text and values in settings menu
+        infRateValue.text = p.infectionRate.ToString("0.00");
     }
 
     int GetStatCount(string studentType)
@@ -56,4 +64,14 @@ public class UIHandler : MonoBehaviour
             return -1;
         }
     }
+
+
+    bool settingsOpen = false;
+
+    public void OpenSettingsMenu()
+    {
+        settingsOpen = !settingsOpen;
+        settingsMenu.SetActive(settingsOpen);
+    }
+
 }
--- GymnasieArbete(utan_ECS)/Assets/scripts/Utils/InfectionParameters.cs
+++ GymnasieArbete(utan_ECS)/Assets/scripts/Utils/InfectionParameters.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 
 public class InfectionParameters : MonoBehaviour
 {
@@ -13,10 +13,32 @@ public class InfectionParameters : MonoBehaviour
     public float infectionRadius;
     [Range(0f, 1f)]
     public float infectionRate;
+    public bool showInfectionRadius;
+
     public float timeTillRemoved;
     public float incubationTime;
     public int population;
     public int initiallyInfected;
     public float dayLength;
-}
 
+    public Slider infRate;
+    public Text infRadius;
+    public Toggle showInfRad;
+
+
+    public void Update()
+    {
+        infectionRate = infRate.value;
+        float n;
+        if (float.TryParse(infRadius.text, out n))
+        {
+            infectionRadius = n;
+        }
+        else
+        {
+            infectionRadius = 1;
+        }
+        showInfectionRadius = showInfRad.isOn;
+    }
+
+}
